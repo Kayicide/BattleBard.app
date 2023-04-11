0dac34317cc9f3aefcece89e1d77d5e2ba1bfa20
Scarlett <Scarlett@kayde.me>
2023-04-11 14:04:42 +0100

    Hooking up tailwind

--- Web/Client/tailwind.config.js
+++ Web/Client/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ["./**/*.{razor,html,cshtml}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
 }
 
